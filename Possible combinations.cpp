6def6b91f6b9603e8367f3843f19adcc484d2d70
rishabh-shukla <shukla.2@iitj.ac.in>
2017-05-23 19:27:19 +0530

    Add files via upload

--- Possible combinations.cpp
+++ Possible combinations.cpp
@@ -1,8 +1,10 @@
 #include<stdio.h>
+#include<algorithm>
 void combinationUtil(int arr[],int n,int r,int index,int data[],int i);
 void printCombination(int arr[], int n, int r)
 {
     int data[r];
+    std::sort(arr,arr+n);
     combinationUtil(arr, n, r, 0, data, 0);
 }
 void combinationUtil(int arr[], int n, int r, int index, int data[], int i)
@@ -18,11 +20,13 @@ void combinationUtil(int arr[], int n, int r, int index, int data[], int i)
         return;
     data[index] = arr[i];
     combinationUtil(arr, n, r, index+1, data, i+1);
+    while(arr[i]==arr[i+1])
+        i++;
     combinationUtil(arr, n, r, index, data, i+1);
 }
 int main()
 {
-    int arr[] = {1, 2, 3, 4, 5};
+    int arr[] = {1, 2, 3, 4, 1};
     int r = 3;
     int n = sizeof(arr)/sizeof(arr[0]);
     printCombination(arr, n, r);
