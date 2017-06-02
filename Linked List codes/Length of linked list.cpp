2bf60ee836446f128384e05600281f79fcf26b1f
rishabh-shukla <shukla.2@iitj.ac.in>
2017-06-02 21:28:10 +0530

    Add files via upload

---
+++ Linked List codes/Length of linked list.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+struct node
+{
+    int data;
+    node* next;
+};
+void insert_front(node** head,int data)
+{
+    struct node* n=new node;
+    n->data=data;
+    n->next=*head;
+    *head=n;
+}
+int len(node* head)
+{
+    node* n=new node;
+    n=head;
+    int l=0;
+    while(n!=NULL)
+    {
+        l++;
+        n=n->next;
+    }
+    return l;
+}
+int main()
+{
+    struct node * head=NULL;
+    insert_front(&head,1);
+    insert_front(&head,2);
+    insert_front(&head,3);
+    insert_front(&head,4);
+    insert_front(&head,1);
+    cout<<len(head);
+}
