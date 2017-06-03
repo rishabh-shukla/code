91b866bf468ba12dfd862b2e3b95ca6de440b39a
rishabh-shukla <shukla.2@iitj.ac.in>
2017-06-03 20:25:19 +0530

    Add files via upload

---
+++ Linked List codes/Reverse List.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+struct node
+{
+    int data;
+    node* next;
+};
+void print(node* n)
+{
+    while(n!=NULL)
+    {
+        cout<<n->data<<" ";
+        n=n->next;
+    }
+}
+void insert_front(node** head,int data)
+{
+    struct node* n=new node;
+    n->data=data;
+    n->next=*head;
+    *head=n;
+}
+int len(node* head)
+{
+    node* n=new node;
+    n=head;
+    int l=0;
+    while(n!=NULL)
+    {
+        l++;
+        n=n->next;
+    }
+    return l;
+}
+int len_rec(node* head)
+{
+    if(head==NULL)
+        return 0;
+    else return 1+len_rec(head->next);
+}
+void reverse(node** head)
+{
+    node* prev=NULL,*curr,*next;
+    curr=*head;
+    while(curr!=NULL)
+    {
+        next=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=next;
+    }
+    *head=prev;
+}
+int main()
+{
+    struct node * head=NULL;
+    insert_front(&head,1);
+    insert_front(&head,2);
+    insert_front(&head,3);
+    insert_front(&head,4);
+    insert_front(&head,1);
+    print(head);
+    cout<<'\n';
+    reverse(&head);
+    print(head);
+}
+
