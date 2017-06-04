63505dbf8ee755f4786111588dd20ab0834fc7ac
rishabh-shukla <shukla.2@iitj.ac.in>
2017-06-04 19:45:13 +0530

    Add files via upload

---
+++ Linked List codes/Swap pairwise.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+struct node
+{
+    int data;
+    node* next;
+};
+void print(node* n)
+{
+    while(n!=NULL)
+    {
+        cout<<n->data<<" ";
+        n=n->next;
+    }
+}
+void insert_front(node** head,int data)
+{
+    struct node* n=new node;
+    n->data=data;
+    n->next=*head;
+    *head=n;
+}
+int len(node* head)
+{
+    node* n=new node;
+    n=head;
+    int l=0;
+    while(n!=NULL)
+    {
+        l++;
+        n=n->next;
+    }
+    return l;
+}
+int len_rec(node* head)
+{
+    if(head==NULL)
+        return 0;
+    else return 1+len_rec(head->next);
+}
+void swap_pair(node* head)
+{
+    if(head!=NULL && head->next!=NULL)
+     {
+        swap(head->data,head->next->data);
+        swap_pair(head->next->next);
+     }
+}
+int main()
+{
+    struct node * head=NULL;
+    insert_front(&head,1);
+    insert_front(&head,2);
+    insert_front(&head,3);
+    insert_front(&head,4);
+    insert_front(&head,1);
+    swap_pair(head);
+    print(head);
+}
