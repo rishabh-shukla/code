72add20ad8bd07cc9d818afdadb674c709883ac7
rishabh-shukla <shukla.2@iitj.ac.in>
2017-06-04 15:53:57 +0530

    Add files via upload

---
+++ Linked List codes/Delete node with only given pointer.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+struct node
+{
+    int data;
+    node* next;
+};
+void print(node* n)
+{
+    while(n!=NULL)
+    {
+        cout<<n->data<<" ";
+        n=n->next;
+    }
+}
+void insert_front(node** head,int data)
+{
+    struct node* n=new node;
+    n->data=data;
+    n->next=*head;
+    *head=n;
+}
+int len(node* head)
+{
+    node* n=new node;
+    n=head;
+    int l=0;
+    while(n!=NULL)
+    {
+        l++;
+        n=n->next;
+    }
+    return l;
+}
+int len_rec(node* head)
+{
+    if(head==NULL)
+        return 0;
+    else return 1+len_rec(head->next);
+}
+void delete_node(node* n)
+{
+    node* t=n->next;
+    n->data=t->data;
+    n->next=t->next;
+    free(t);
+}
+int main()
+{
+    struct node * head=NULL;
+    insert_front(&head,1);
+    insert_front(&head,2);
+    insert_front(&head,3);
+    insert_front(&head,4);
+    insert_front(&head,1);
+    print(head);
+    cout<<'\n';
+    delete_node(head->next);
+    print(head);
+}
+
