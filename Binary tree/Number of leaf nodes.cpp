07594a89e3eada1650bb90c738b25c2d9c2c4f62
rishabh-shukla <shukla.2@iitj.ac.in>
2017-06-21 21:17:14 +0530

    Add files via upload

---
+++ Binary tree/Number of leaf nodes.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+struct node
+{
+    int data;
+    node *left,*right;
+};
+int height(node *root)
+{
+    if(root==NULL)
+        return 0;
+    else return max(height(root->left),height(root->right))+1;
+}
+node* newNode(int data)
+{
+    node* n=new node;
+    n->data=data;
+    n->left=NULL;
+    n->right=NULL;
+    return n;
+}
+void deleteTree(node* root)
+{
+    if(root==NULL)
+        return;
+    deleteTree(root->left);
+    deleteTree(root->right);
+    cout<<root->data<<" ";
+    free(root);
+}
+int countleaf(node* root)
+{
+    if(root==NULL)
+        return 0;
+    if(root->left==NULL && root->right==NULL)
+        return 1;
+    else
+        return countleaf(root->left)+countleaf(root->right)+1;
+}
+int main()
+{
+    struct node *root = newNode(1);
+
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+    cout<<countleaf(root);
+
+}
