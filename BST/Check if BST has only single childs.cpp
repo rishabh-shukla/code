f7eb577a38b873445fb2238f7cd474885731367d
rishabh-shukla <shukla.2@iitj.ac.in>
2017-07-04 19:45:21 +0530

    Add files via upload

---
+++ BST/Check if BST has only single childs.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+bool hasOnlyOneChild(int pre[], int size)
+{
+    int nextDiff, lastDiff;
+
+    for (int i=0; i<size-1; i++)
+    {
+        nextDiff = pre[i] - pre[i+1];
+        lastDiff = pre[i] - pre[size-1];
+        if (nextDiff*lastDiff < 0)
+            return false;;
+    }
+    return true;
+}
+int main()
+{
+    int pre[] = {8, 3, 5, 7, 6};
+    int size = sizeof(pre)/sizeof(pre[0]);
+    if (hasOnlyOneChild(pre, size) == true )
+        printf("Yes");
+    else
+        printf("No");
+}
