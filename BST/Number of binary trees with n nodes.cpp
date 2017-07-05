3ebaab7cb269686d0b02bdd8c9986bbf181c7085
rishabh-shukla <shukla.2@iitj.ac.in>
2017-07-05 18:05:45 +0530

    Add files via upload

---
+++ BST/Number of binary trees with n nodes.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+unsigned long int binomialCoeff(unsigned int n, unsigned int k)
+{
+    unsigned long int res = 1;
+    if (k > n - k)
+        k = n - k;
+
+    for (int i = 0; i < k; ++i)
+    {
+        res *= (n - i);
+        res /= (i + 1);
+    }
+
+    return res;
+}
+unsigned long int catalan(unsigned int n)
+{
+    unsigned long int c = binomialCoeff(2*n, n);
+
+    return c/(n+1);
+}
+int main()
+{
+    int n=5;
+    cout<<catalan(n);
+}
