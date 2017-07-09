d67e4dc31c3212981d203ff1ec1011155cb559a8
rishabh-shukla <shukla.2@iitj.ac.in>
2017-07-09 18:38:03 +0530

    Add files via upload

---
+++ Hashing/Find pairs such that sum is same.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool findPairs(int arr[], int n)
+{
+    map<int, pair<int, int> > Hash;
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = i + 1; j < n; ++j)
+        {
+            int sum = arr[i] + arr[j];
+            if (Hash.find(sum) == Hash.end())
+                Hash[sum] = make_pair(i, j);
+            else
+            {
+                pair<int, int> pp = Hash[sum];
+                cout << "(" << arr[pp.first] << ", " << arr[pp.second]
+                     << ") and (" << arr[i] << ", " << arr[j] << ")\n";
+                return true;
+            }
+        }
+    }
+
+    cout << "No pairs found";
+    return false;
+}
+int main()
+{
+    int arr[] = {3, 4, 7, 1, 2, 9, 8};
+    int n  =  sizeof arr / sizeof arr[0];
+    findPairs(arr, n);
+    return 0;
+}
